393ac69f4248dcd3694d12127b3bdd0c6d840211
cacdesign <pro.nicolasfernandez@gmail.com>
2019-01-11 20:23:48 +0100

    Add files via upload

---
+++ index.rb
@@ -0,0 +1,30 @@
+def index1
+
+	puts "Choisis ta question jeune Padawan"
+	puts " "
+	puts "1. Combien y a-t-il de journalistes dans cet array ?"
+	puts "2. Combien d'handle contiennent un numéro ?"
+	puts "3. Combien d'handle contiennent les 4 lettres du prénom Aude à la suite (sans prendre en compte les majuscules) ?"
+	puts "4. Combien commencent par une majuscule (première lettre juste après le @) ?"
+	puts "5. Combien contiennent une majuscule ?"
+	puts "6. Combien y a-t-il de underscore _ dans tous les pseudos confondus ?"
+	puts "7. Trie la liste de handle par ordre alphabétique."
+	puts "8. Quels sont les 50 handles les plus courts de ce array ?"
+	puts "9. Quelle est la position dans l'array de la personne @epenser ?"
+
+end
+
+def index_crypto
+
+	puts "Choisis ta question jeune Padawan"
+	puts " "
+	puts "1. La ou les crypto qui ont la plus grosse valeur."
+	puts "2. La ou les crypto qui ont la plus petite valeur."
+	puts "3. Le nombre de crypto contenant le mot coin. ?"
+	puts "4. Les devises, dont le cours est inférieur à 6000 (Indice : on peut comparer en valeur 2 integers mais pas 2 strings. Pense bien à enlever le $ et éventuellement utiliser .to_i pour faire cet exercice)."
+	puts "5. La devise la plus chère parmi celles dont le cours est inférieur à 6000."
+
+end
+
+
+
